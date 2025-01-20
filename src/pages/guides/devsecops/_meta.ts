5c870787f6cd20484420cfdc9990badb0c6adace
Rafi <refaei.shikho@hotmail.com>
2025-01-20 17:34:42 +0100

    add DevSecOps guide
    Signed-off-by: Rafi <refaei.shikho@hotmail.com>

---
+++ src/pages/guides/devsecops/_meta.ts
@@ -0,0 +1,11 @@
+export default {
+    intoto: { title: 'In-toto' },
+    sast: { title: 'Static Application Security Testing (SAST)' },
+    dast: { title: 'Dynamic Application Security Testing (DAST)' },
+    'secret-scanning': { title: 'Secret Scanning' },
+    ias: { title: 'Interactive Application Security Testing (IAST)' },
+    'container-scanning': { title: 'Container Scanning' },
+    'software-composition-analysis': {
+        title: 'Software Composition Analysis(SCA)',
+    },
+}
