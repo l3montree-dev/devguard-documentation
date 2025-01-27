9e760084955d3424d5f723a94c7cf1464093594e
Rafi <refaei.shikho@hotmail.com>
2025-01-27 17:18:42 +0100

    add 'Manage Risks' section and update links in ISO guide for consistency

--- src/pages/guides/_meta.ts
+++ src/pages/guides/_meta.ts
@@ -3,4 +3,5 @@ export default {
     risksMtigation: { title: 'Risks Mitigation' },
     iso: { title: 'ISO 27001' },
     cra: { title: 'Cyber Resiliance Act' },
+    mangeRisks: { title: 'Manage Risks' },
 }
