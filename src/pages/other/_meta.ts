f3351fdc375b82cd913b546dca031528f13c48dc
Rafi <refaei.shikho@hotmail.com>
2025-05-13 18:26:53 +0200

    Add GitHub workflows documentation
    Signed-off-by: Rafi <refaei.shikho@hotmail.com>

--- src/pages/other/_meta.ts
+++ src/pages/other/_meta.ts
@@ -1,6 +1,14 @@
 export default {
     index: { title: 'About DevGuards Creators' },
     'sponsors-partners': { title: 'Sponsors & Partners' },
+
+    'github-workflows': {
+        title: 'GitHub Workflows',
+    },
+    'gitlab-components': {
+        title: 'GitLab Components',
+    },
+
     concepts: { title: 'Concepts' },
     faq: { title: 'FAQ' },
 }
---
+++ src/pages/other/github-workflows/_meta.ts
@@ -0,0 +1,34 @@
+// Copyright 2025 rafaeishikho.
+// SPDX-License-Identifier: Apache-2.0
+export default {
+    'secret-scanning': {
+        title: 'Secret Scanning',
+    },
+    sast: {
+        title: 'Static Application Security Testing',
+    },
+    iac: {
+        title: 'Infrastructure as Code',
+    },
+    sca: {
+        title: 'Software Composition Analysis',
+    },
+    'container-scanning': {
+        title: 'Container Scanning',
+    },
+    'build-image': {
+        title: 'Build Image',
+    },
+    sign: {
+        title: 'Sign Image',
+    },
+    deploy: {
+        title: 'Deploy Image',
+    },
+    'dependency-risk-identification': {
+        title: 'Dependency Risk Identification',
+    },
+    'code-risk-identification': {
+        title: 'Code Risk Identification',
+    },
+}
