fd6b81831ead0326cf1052915fe0bc7dc318fdf8
Tim Bastin <bastin.tim@gmail.com>
2025-05-16 14:11:08 +0200

    improves getting started

--- src/pages/getting-started/_meta.ts
+++ src/pages/getting-started/_meta.ts
@@ -1,6 +1,6 @@
 export default {
-    index: { title: 'Overview' },
-    'local-quickstart': { title: 'Local Quickstart' },
+    index: { title: 'Quickstart' },
+    'local-demo': { title: 'Local Demo' },
     'self-hosting-devguard': { title: 'Self Hosting DevGuard' },
     'setup-gitlab-integration': { title: 'Setup GitLab Integration' },
     'setup-github-integration': { title: 'Setup GitHub Integration' },
