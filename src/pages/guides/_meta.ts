89f511d20a5b15d2954ea685dc4123a32ff1f5ee
Sebastian Kawelke <sebastian.kawelke@l3montree.com>
2025-09-02 13:52:20 +0200

    Reworks container hardening starting points
    Signed-off-by: Sebastian Kawelke <sebastian.kawelke@l3montree.com>

--- src/pages/guides/_meta.ts
+++ src/pages/guides/_meta.ts
@@ -3,4 +3,7 @@ export default {
     'several-assets-and-artifacts-to-devguard-monorepos': {
         title: 'Mapping Several Assets and Artifacts to DevGuard (e.g., Monorepos)',
     },
+    'container-hardening-starting-points': {
+        title: 'Container Hardening Starting Points',
+    },
 }
