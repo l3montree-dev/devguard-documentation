916d930611936900cffba2ce4b13ce90ba3c7261
Frederic Noppe <frederic.noppe@l3montree.com>
2025-09-24 11:56:42 +0200

    changes the logo banner and adds cps logo
    Signed-off-by: Frederic Noppe <frederic.noppe@l3montree.com>

--- src/components/top-level-pages/logo-line.tsx
+++ src/components/top-level-pages/logo-line.tsx
@@ -3,71 +3,135 @@ import { cn } from '../../lib/utils'
 
 export function LogoLine({ inverted = false }: { inverted?: boolean }) {
     return (
-        <div
-            className={
-                'flex justify-between max-sm:mx-auto max-sm:max-w-md max-sm:flex-wrap max-sm:justify-evenly max-sm:gap-x-4 max-sm:gap-y-8'
-            }
-        >
-            <Image
-                alt="OWASP Logo"
-                src="/sponsors/sp-owasp.svg"
-                className={cn(
-                    inverted && 'invert',
-                    'h-9 w-auto max-sm:mx-auto sm:h-8 lg:h-11',
-                )}
-                width={300}
-                height={100}
-            />
-            <Image
-                alt="Ikor part of X1 Logo"
-                src="/sponsors/sp-ikor.svg"
-                className={cn(
-                    inverted && 'invert',
-                    'h-9 w-auto max-sm:mx-auto sm:h-8 lg:h-11',
-                )}
-                width={300}
-                height={100}
-            />
-            <Image
-                alt="Bonn-Rhein-Sieg University of Applied Sciences Logo"
-                src="/sponsors/sp-hbrs.svg"
-                className={cn(
-                    inverted && 'invert',
-                    'h-9 w-auto max-sm:mx-auto sm:h-8 lg:h-11',
-                )}
-                width={300}
-                height={100}
-            />
-            <Image
-                alt="University of Giessen Logo"
-                src="/sponsors/sp-jlu.svg"
-                className={cn(
-                    inverted && 'invert',
-                    'h-9 w-auto max-sm:mx-auto sm:h-8 lg:h-11',
-                )}
-                width={300}
-                height={100}
-            />
-            <Image
-                alt="Wheregroup Logo"
-                src="/sponsors/sp-wheregroup.svg"
-                className={cn(
-                    inverted && 'invert',
-                    'h-9 w-auto max-sm:mx-auto sm:h-8 lg:h-11',
-                )}
-                width={300}
-                height={100}
-            />
-            <Image
-                alt="BusinessCode Logo"
-                src="/sponsors/bc-logo-white.png"
-                className={cn(
-                    inverted && 'invert',
-                    'h-9 w-auto max-sm:mx-auto sm:h-8',
-                )}
-                width={300}
-                height={100}
-            />
+        <div className="flex flex-wrap justify-center gap-x-[100px] gap-y-[100px] max-sm:gap-x-4 max-sm:gap-y-4">
+            <a
+                href="https://owasp.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <Image
+                    alt="OWASP Logo"
+                    src="/sponsors/sp-owasp.svg"
+                    className={cn(inverted && 'invert')}
+                    width={300}
+                    height={100}
+                    style={{
+                        flex: '0 0 auto',
+                        width: '200px',
+                        height: '100px',
+                    }}
+                />
+            </a>
+
+            <a
+                href="https://ikor.one"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <Image
+                    alt="Ikor Logo"
+                    src="/sponsors/sp-ikor.svg"
+                    className={cn(inverted && 'invert')}
+                    width={300}
+                    height={100}
+                    style={{
+                        flex: '0 0 auto',
+                        width: '200px',
+                        height: '100px',
+                    }}
+                />
+            </a>
+
+            <a
+                href="https://h-brs.de"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <Image
+                    alt="HBRS Logo"
+                    src="/sponsors/sp-hbrs.svg"
+                    className={cn(inverted && 'invert')}
+                    width={300}
+                    height={100}
+                    style={{
+                        flex: '0 0 auto',
+                        width: '200px',
+                        height: '100px',
+                    }}
+                />
+            </a>
+
+            <a
+                href="https://www.uni-giessen.de/de"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <Image
+                    alt="JLU Logo"
+                    src="/sponsors/sp-jlu.svg"
+                    className={cn(inverted && 'invert')}
+                    width={300}
+                    height={100}
+                    style={{
+                        flex: '0 0 auto',
+                        width: '200px',
+                        height: '100px',
+                    }}
+                />
+            </a>
+
+            <a
+                href="https://wheregroup.com"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <Image
+                    alt="Wheregroup Logo"
+                    src="/sponsors/sp-wheregroup.svg"
+                    className={cn(inverted && 'invert')}
+                    width={300}
+                    height={100}
+                    style={{
+                        flex: '0 0 auto',
+                        width: '200px',
+                        height: '100px',
+                    }}
+                />
+            </a>
+
+            <a
+                href="https://businesscode.de"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <Image
+                    alt="BusinessCode Logo"
+                    src="/sponsors/bc-logo-white.png"
+                    className={cn(inverted && 'invert')}
+                    width={300}
+                    height={100}
+                    style={{ flex: '0 0 auto', width: '200px', height: '40px' }}
+                />
+            </a>
+
+            <a
+                href="https://www.cps-it.de/"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <Image
+                    alt="CPS Logo"
+                    src="/sponsors/CPS_Logo_weiß.png"
+                    className={cn(inverted && 'invert')}
+                    width={300}
+                    height={100}
+                    style={{
+                        flex: '0 0 auto',
+                        width: '200px',
+                        height: '100px',
+                    }}
+                />
+            </a>
         </div>
     )
 }
