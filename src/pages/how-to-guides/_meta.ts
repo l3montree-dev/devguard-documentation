9caed9e582bcab5a1caa985b98d0ef00ec23b8b9
Sebastian Kawelke <sebastian.kawelke@l3montree.com>
2026-01-04 21:21:51 +0100

    Adds how to for scanning oci images, moves some files
    Signed-off-by: Sebastian Kawelke <sebastian.kawelke@l3montree.com>

--- src/pages/how-to-guides/_meta.ts
+++ src/pages/how-to-guides/_meta.ts
@@ -1,17 +1,6 @@
 export default {
     index: { title: 'Overview' },
-    'ingesting-upstream-information': {
-        title: 'Ingesting Upstream Information',
-    },
-    'csaf-common-security-advisory-framework': {
-        title: 'CSAF (Common Security Advisory Framework)',
-    },
-    'discover-base-image-attestations': {
-        title: 'Discovering Base Image Attestations',
-    },
-    'several-assets-and-artifacts-to-devguard-monorepos': {
-        title: 'Mapping Several Assets and Artifacts (e.g., Monorepos)',
-    },
+    scanning: { title: 'Scanning' },
     'dependency-proxy': {
         title: 'Dependency Proxy with Malicious Package Firewall',
     },
@@ -19,5 +8,4 @@ export default {
         title: 'Container Hardening Guide',
     },
     'risk-mitigation-guides': { title: 'Risk Mitigation Guides' },
-    'explaining-sboms': { title: 'Explaining SBOMs' },
 }
---
+++ src/pages/how-to-guides/scanning/_meta.ts
@@ -0,0 +1,8 @@
+export default {
+    'scan-docker-images': { title: 'Scan OCI Images' },
+    'scan-source-code': { title: 'Scan Source Code' },
+    'scan-dependencies': { title: 'Scan Dependencies' },
+    'upload-sbom': { title: 'Upload SBOM' },
+    'upload-vex': { title: 'Upload VEX' },
+    'scheduled-scans': { title: 'Scheduled Scans' },
+}
