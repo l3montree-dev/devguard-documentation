eb93329a08b8cbee2dd6fc5b6665eea87f19ef05
rafi <refaei.shikho@hotmail.com>
2026-01-12 16:25:38 +0100

    update how to scanning guides
    Signed-off-by: rafi <refaei.shikho@hotmail.com>

--- src/pages/how-to-guides/scanning/_meta.ts
+++ src/pages/how-to-guides/scanning/_meta.ts
@@ -2,6 +2,8 @@ export default {
     'scan-docker-images': { title: 'Scan OCI Images' },
     'scan-source-code': { title: 'Scan Source Code' },
     'scan-dependencies': { title: 'Scan Dependencies' },
+    'scan-with-github-actions': { title: 'Scan with GitHub Actions' },
+    'scan-with-gitlab-ci': { title: 'Scan with GitLab CI' },
     'upload-sbom': { title: 'Upload SBOM' },
     'upload-vex': { title: 'Upload VEX' },
     'scheduled-scans': { title: 'Scheduled Scans' },
