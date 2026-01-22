fac16da8ffd14d70edf09d30bbbe05d649c6e402
Philipp Schoenbach <philippschoenbach1@gmail.com>
2026-01-22 20:47:55 +0100

    supply-chain-security final changes for now, more styling later

--- src/pages/explanations/supply-chain-security/_meta.ts
+++ src/pages/explanations/supply-chain-security/_meta.ts
@@ -2,9 +2,19 @@ export default {
     'what-is-supply-chain-security': {
         title: 'What is Supply Chain Security?',
     },
-    'in-toto-framework': { title: 'in-toto Framework' },
-    attestations: { title: 'Attestations' },
-    'supply-chain-verification': { title: 'Supply Chain Verification' },
-    'slsa-framework': { title: 'SLSA Framework' },
-    'provenance-tracking': { title: 'Provenance Tracking' },
+    'in-toto-framework': {
+        title: 'In-Toto Framework',
+    },
+    attestations: {
+        title: 'Attestations',
+    },
+    'supply-chain-verification': {
+        title: 'Supply Chain Verification',
+    },
+    'slsa-framework': {
+        title: 'SLSA Framework',
+    },
+    'provenance-tracking': {
+        title: 'Provenance Tracking',
+    },
 }
