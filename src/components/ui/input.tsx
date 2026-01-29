72e1480c3095c7ca31d51310eb168f92ec1acf48
Julian Kepka <julian.kepka@l3montree.com>
2026-01-29 13:32:55 +0100

    skeleton load started

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,19 +1,26 @@
 import * as React from 'react'
+import { Search } from 'lucide-react'
 
 import { cn } from '@/lib/utils'
 
 function Input({ className, type, ...props }: React.ComponentProps<'input'>) {
     return (
-        <input
-            type={type}
-            placeholder="Search vulnerability..."
-            data-slot="input"
-            className={cn(
-                'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:aria-invalid:border-destructive/50 aria-invalid:ring-[3px] h-12 w-full min-w-0 rounded-xl border border-input bg-transparent px-2.5 py-1 text-base outline-none transition-colors file:inline-flex file:h-6 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-input/50 disabled:opacity-50 dark:bg-input/30 dark:disabled:bg-input/80 md:text-lg',
-                className,
-            )}
-            {...props}
-        />
+        <div className="group relative">
+            <Search
+                className="absolute left-3 h-5 w-5 text-muted-foreground"
+                style={{ top: '2.125rem' }}
+            />
+            <input
+                type={type}
+                placeholder="Search vulnerability..."
+                data-slot="input"
+                className={cn(
+                    'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:aria-invalid:border-destructive/50 aria-invalid:ring-[3px] h-12 w-full min-w-0 rounded-xl border border-input bg-transparent py-0 pl-10 pr-2.5 text-base outline-none transition-colors file:inline-flex file:h-6 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-input/50 disabled:opacity-50 dark:bg-input/30 dark:disabled:bg-input/80 md:text-lg',
+                    className,
+                )}
+                {...props}
+            />
+        </div>
     )
 }
 
