cc6fe0b888e2d40d6d6f25265dc3ec885e0c19d4
Julian Kepka <julian.kepka@l3montree.com>
2026-02-10 09:41:03 +0100

    sitemap new load

--- src/pages/api/sitemap.xml.ts
+++ src/pages/api/sitemap.xml.ts
@@ -11,10 +11,6 @@ export default async function handler(
 
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <sitemap>
-    <loc>${baseUrl}/sitemap-0.xml</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
-  </sitemap>
 ${Array.from(
     { length: numberOfCveSitemaps },
     (_, i) => `  <sitemap>
