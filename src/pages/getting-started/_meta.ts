c9d1077133dba201f6e26b7435cb562d8611c51a
Frédéric Noppe <frederic.noppe@l3montree.com>
2026-02-17 12:15:14 +0100

    adds key Concepts in 2 minutes page
    Signed-off-by: Frédéric Noppe <frederic.noppe@l3montree.com>

--- src/pages/getting-started/_meta.ts
+++ src/pages/getting-started/_meta.ts
@@ -2,6 +2,6 @@ export default {
     index: { title: 'Quickstart' },
     installation: { title: 'Installation' },
     'first-scan': { title: 'Your First Scan' },
-    'key-concepts': { title: "DevGuard's Key Concepts in 5 Minutes" },
+    'key-concepts': { title: "DevGuard's Key Concepts in 2 Minutes" },
     'choose-your-path': { title: 'Choose Your Path' },
 }
