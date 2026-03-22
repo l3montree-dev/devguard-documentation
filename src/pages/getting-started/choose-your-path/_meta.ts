ad85372131d6c8feda03cbad8c1031591313cf31
Sebastian Kawelke <sebastian.kawelke@l3montree.com>
2026-03-22 13:14:04 +0100

    Adds for devops
    Signed-off-by: Sebastian Kawelke <sebastian.kawelke@l3montree.com>

---
+++ src/pages/getting-started/choose-your-path/_meta.ts
@@ -0,0 +1,6 @@
+export default {
+    'for-security-teams': { title: 'For Security Teams' },
+    'for-devops': { title: 'For DevOps' },
+    'for-developers': { title: 'For Developers' },
+    'for-compliance-officers': { title: 'For Compliance Officers' },
+}
