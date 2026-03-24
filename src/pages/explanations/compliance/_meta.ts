1abcb9b7f0b951c4c7f2aab5f34a6154c887e1b8
Tim Bastin <bastin.tim@gmail.com>
2026-03-24 09:41:41 +0100

    adds documentation for devguard and the cra

--- src/pages/explanations/compliance/_meta.ts
+++ src/pages/explanations/compliance/_meta.ts
@@ -1,6 +1,9 @@
 export default {
     'why-compliance-matters': { title: 'Why Compliance Matters' },
     'cyber-resilience-act': { title: 'Cyber Resiliance Act' },
+    'cra-open-source-steward': { title: 'CRA: Open Source Steward' },
+    'cra-manufacturer': { title: 'CRA: Manufacturer' },
+    'cra-maintainer': { title: 'CRA: Maintainer' },
     'iso-27001-mapping': { title: 'ISO 27001' },
     'csaf-vex-explained': { title: 'CSAF and VEX' },
     'sbom-standards': { title: 'CycloneDX vs SPDX' },
