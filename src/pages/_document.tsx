8fae370f3eddefd1ace7a24b0cf23ff1fa108b79
Konstantin Zhukov <zhukov.konst.sl@gmail.com>
2026-05-08 19:06:48 +0200

    add chat widget

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
     return (
         <Html data-kern-theme="dark" lang="en" style={{ colorScheme: 'dark' }}>
-            <Head />
+            <Head>
+                <script src="https://devguard.org/chat-widget.js" data-library="/websites/devguard" async />
+            </Head>
             <body>
                 <Main />
                 <NextScript />
