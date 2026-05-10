1ea5d23a3e6ecbf3919940e844d774a482fd4a44
Sebastian Kawelke <sebastian.kawelke@l3montree.com>
2026-05-10 12:12:09 +0200

    Makes chat widget souce configurable, adds privacy notice, adds to image args
    Signed-off-by: Sebastian Kawelke <sebastian.kawelke@l3montree.com>

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,23 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
+    const chatWidgetSrc = process.env.NEXT_PUBLIC_CHAT_WIDGET_SRC
+    const chatWidgetIntegrity = process.env.NEXT_PUBLIC_CHAT_WIDGET_INTEGRITY
+
     return (
         <Html data-kern-theme="dark" lang="en" style={{ colorScheme: 'dark' }}>
             <Head>
-                <script src="https://devguard.org/chat-widget.js" data-library="/websites/devguard" async />
+                {chatWidgetSrc && (
+                    <script
+                        src={chatWidgetSrc}
+                        data-library="/websites/devguard"
+                        async
+                        {...(chatWidgetIntegrity && {
+                            integrity: chatWidgetIntegrity,
+                            crossOrigin: 'anonymous',
+                        })}
+                    />
+                )}
             </Head>
             <body>
                 <Main />
