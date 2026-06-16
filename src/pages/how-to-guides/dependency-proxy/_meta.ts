c16e21c76dfc72959d858215e46810c9ae39c651
Tim Bastin <bastin.tim@gmail.com>
2026-06-16 10:45:52 +0200

    improves structure, adds information about devguard bearer token

--- src/pages/how-to-guides/dependency-proxy/_meta.ts
+++ src/pages/how-to-guides/dependency-proxy/_meta.ts
@@ -1,6 +1,6 @@
 export default {
+    index: { title: 'Overview' },
     'cache-management': { title: 'Cache Management' },
-    'malicious-package-blocking': { title: 'Malicious Package Blocking' },
     'setup-go-proxy': { title: 'Setup Go Proxy' },
     'setup-npm-proxy': { title: 'Setup NPM Proxy' },
     'setup-pypi-proxy': { title: 'Setup PyPI Proxy' },
