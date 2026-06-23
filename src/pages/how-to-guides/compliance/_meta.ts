ecdd2e9ca139f56175af9089138f420e8fa361c7
Philipp Schoenbach <philippschoenbach1@gmail.com>
2026-06-23 21:00:49 +0200

    fix missing sidebar meta.ts entries and dead links due to DWT not handling .mdx endings in URLs

--- src/pages/how-to-guides/compliance/_meta.ts
+++ src/pages/how-to-guides/compliance/_meta.ts
@@ -1,3 +1,7 @@
 export default {
     'compliance-dashboards': { title: 'View Compliance Dashboards' },
+    'audit-logs': { title: 'Audit Logs' },
+    'export-sbom': { title: 'Export SBOM' },
+    'generate-csaf-reports': { title: 'Generate CSAF Reports' },
+    'generate-vex-documents': { title: 'Generate VEX Documents' }
 }
