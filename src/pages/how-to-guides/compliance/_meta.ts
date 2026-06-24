3c22e29eb72630d7b8c5742ac0f6212df30e8c9e
Philipp Schoenbach <philippschoenbach1@gmail.com>
2026-06-24 14:27:43 +0200

    fixes for seo check

--- src/pages/how-to-guides/compliance/_meta.ts
+++ src/pages/how-to-guides/compliance/_meta.ts
@@ -3,5 +3,6 @@ export default {
     'audit-logs': { title: 'Audit Logs' },
     'export-sbom': { title: 'Export SBOM' },
     'generate-csaf-reports': { title: 'Generate CSAF Reports' },
-    'generate-vex-documents': { title: 'Generate VEX Documents' }
+    'generate-vex-documents': { title: 'Generate VEX Documents' },
+    'attestation-policies': { title: 'Manage Compliance & Attestation Policies' }
 }
