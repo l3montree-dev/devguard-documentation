38478008819419abdede43d3f471189da338091b
Dboy0ZDev <83472928+Dboy0ZDev@users.noreply.github.com>
2026-07-03 15:52:12 +0200

    Added Documentation page for vs code extension poc

--- src/pages/how-to-guides/integrations/_meta.ts
+++ src/pages/how-to-guides/integrations/_meta.ts
@@ -3,6 +3,7 @@ export default {
     'custom-webhooks': { title: 'Custom Webhooks' },
     'webhook-events': { title: 'Webhook Events' },
     'webhook-security': { title: 'Webhook Security' },
+    'devguard-for-vscode': { title: 'DevGuard for VS Code' },
     github: { title: 'GitHub Integration' },
     gitlab: { title: 'GitLab Integration' },
     jira: { title: 'Jira Integration' },
