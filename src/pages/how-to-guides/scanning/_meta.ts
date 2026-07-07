e95fb74799bed5b3bf656b46bf8e424e377466d3
Julian Kepka <julian.kepka@l3montree.com>
2026-07-07 12:15:23 +0200

    feat: added new artifacts how to guide page

--- src/pages/how-to-guides/scanning/_meta.ts
+++ src/pages/how-to-guides/scanning/_meta.ts
@@ -2,6 +2,7 @@ export default {
     'scan-docker-images': { title: 'Scan OCI Images' },
     'scan-source-code': { title: 'Scan Source Code' },
     'scan-dependencies': { title: 'Scan Dependencies' },
+    'branches-tags-and-artifacts': { title: 'Branches, Tags & Artifacts' },
     'scan-with-github-actions': { title: 'Scan with GitHub Actions' },
     'scan-with-gitlab-ci': { title: 'Scan with GitLab CI' },
     'upload-sbom': { title: 'Upload SBOM' },
