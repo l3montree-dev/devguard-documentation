bdd07e319fb2a596c0cc64e5497622a64763d3d5
Frédéric Noppe <frederic.noppe@l3montree.com>
2026-08-19 12:12:15 +0200

    initial documentation
    Signed-off-by: Frédéric Noppe <frederic.noppe@l3montree.com>

--- src/pages/explanations/compliance/_meta.ts
+++ src/pages/explanations/compliance/_meta.ts
@@ -1,11 +1,10 @@
 export default {
     'why-compliance-matters': { title: 'Why Compliance Matters' },
     'cyber-resilience-act': { title: 'Cyber Resiliance Act' },
+    'cra-compliant-vulnerability-disclosure': { title: 'CRA Compliant Vulnerability Disclosure' },
     'iso-27001-mapping': { title: 'ISO 27001' },
     'csaf-vex-explained': { title: 'CSAF and VEX' },
     'sbom-standards': { title: 'CycloneDX vs SPDX' },
     'audit-trails': { title: 'Audit Logging' },
-    'compliance-as-code': {
-        title: 'Compliance as Code',
-    },
+    'compliance-as-code': { title: 'Compliance as Code'},
 }
