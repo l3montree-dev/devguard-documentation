3f22330019e4667e1e5d8b0c978ca2c525ed5d17
Frédéric Noppe <frederic.noppe@l3montree.com>
2026-09-23 10:27:58 +0200

    deletes attestation page from meta.ts
    Signed-off-by: Frédéric Noppe <frederic.noppe@l3montree.com>

--- src/pages/how-to-guides/compliance/_meta.ts
+++ src/pages/how-to-guides/compliance/_meta.ts
@@ -1,9 +1,6 @@
 export default {
     'audit-logs': { title: 'Audit Logs' },
     'export-sbom': { title: 'Export SBOM' },
-    'attestation-policies': {
-        title: 'Manage Compliance & Attestation Policies',
-    },
     'track-compliance-postures': { title: 'Track Compliance Postures' },
     'audit-trails': { title: 'DevGuard Audit Trails' },
 }
